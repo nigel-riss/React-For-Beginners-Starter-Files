99bf705361330dc87b8c0ff7727bd371a74602a4
nigel-riss <mr.kurenkov@gmail.com>
2019-01-08 16:11:25 +0200

    Add first component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,2 +1,7 @@
-// let's go!
-// alert("It's alive!");
+import React from 'react';
+import { render } from 'react-dom';
+
+import StorePicker from './components/StorePicker';
+
+
+render(<StorePicker/>, document.querySelector('#main'));
